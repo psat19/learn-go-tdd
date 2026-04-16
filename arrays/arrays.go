4cce1a64c25127625aa3193e7379bc6a5ff9b729
psat19 <spratza@outlook.com>
2026-04-16 16:05:27 +0530

    Sums continued

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -7,3 +7,28 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbers ...[]int) []int {
+	var sumAll []int
+
+	for _, number := range numbers {
+		sumAll = append(sumAll, Sum(number))
+	}
+
+	return sumAll
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+
+	}
+
+	return sums
+}
